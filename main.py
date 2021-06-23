429ab2442ff085307afffa7f6061c70b33242cc3
eunsu28 <eunsuvg@gmail.com>
2021-06-23 17:29:05 +0800

    hello

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import os 
+from time import sleep as s
 def clear():
     return os.system("clear")
 
-    
+running = True
+while running:
+    a = input("입력해 주세요: ")
+    print("a")
+    s(1)
+    clear()
+    if a == "log out":
+        running = False
+
+print("끝!")
